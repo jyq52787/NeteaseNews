22021135036697fbc54816cff8af2cd6add1f35b
jyq <546017410@.com>
2015-10-26 10:10:01 +0800

    纯代码,在视频中心添加一个方块

---
+++ News/News/photoSeletorViewController.swift
@@ -0,0 +1,55 @@
+//
+//  photoSeletorViewController.swift
+//  照片选择器
+//
+//  Created by 贾永强 on 15/10/20.
+//  Copyright © 2015年 heima. All rights reserved.
+//
+
+import UIKit
+
+class photoSeletorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpImageV1()
+        
+        
+        
+    }
+    func setUpImageV1(){
+        
+        let size = CGSizeMake(200, 200)
+        
+        let imageView = UIImageView()
+        imageView.center = view.center
+        imageView.bounds.size = size
+        imageView.backgroundColor = UIColor.redColor()
+        view.addSubview(imageView)
+        
+        
+        print(imageView.center)
+        
+    }
+    
+    
+    
+    func setUpImageV2(){
+        
+        let center = CGPointMake(UIScreen.mainScreen().bounds.width / 2, UIScreen.mainScreen().bounds.height / 2)
+        let size = CGSizeMake(200, 200)
+        
+        let imageView = UIImageView()
+        imageView.center = center
+        imageView.bounds.size = size
+        imageView.backgroundColor = UIColor.redColor()
+        
+        view.addSubview(imageView)
+        print(imageView.center)
+        print(center)
+        print(imageView)
+        
+        
+    }
+}
